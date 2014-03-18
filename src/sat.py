b4dea3d12ecd8ca6c9d5071ecba66c43900f57fa
Luka Krapic <luka.krapic@student.fmf.uni-lj.si>
2014-03-18 11:17:19 +0100

    Add comments for sat solver implementation in sat.py.

--- src/sat.py
+++ src/sat.py
@@ -1,6 +1,52 @@
 from logConstructs import *
 
 
+class SAT_solver:
+
+    @staticmethod
+    def up(formula):
+        #forall Or nodes with size of clause == 1 add variable name and value to dictionary
+        #if variable has True and False add special value
+        #return dictionary
+        pass
+
+    @staticmethod
+    def purge(formula):
+        pass
+
+    def solve(formula):
+        #convert to NNF
+        #convert to CNF
+        #call solve_cnf
+        pass
+
+    def solve_cnf(formula):
+        #while call up != []
+            #dictinary = up(formula)
+            #if dictionary contains special value return False
+            #formula = formula.evaluate(dictionary)
+
+        #while call purge != []
+            #dictionary = purge(formula)
+            #formula = formula.evaluate(dictionary)
+
+        #scan for remaining variables
+
+        #=============================================
+        #multithreading - spliting search
+        #=============================================
+        #heuristic - some heuristic for variable selection
+        #=============================================
+        #select a variable v
+        #set v to false, evaluate and call solve_cnf
+        #if return == True return True
+        #set v to true, evaluate and call solve_cnf
+        #if return == True return True
+        #return False
+
+        pass
+
+
 def evaluateFormula(expr, varlist, assignments={}):
     if len(varlist) == 0:
 
